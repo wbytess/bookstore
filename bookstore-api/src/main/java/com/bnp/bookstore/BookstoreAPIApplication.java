c2a410b7e4f04b194b5b003cdfb2128d67356b58
Muhammad imran <iimrankhn@gmail.com>
2026-02-02 21:43:08 +0100

    Initial project structure

---
+++ bookstore-api/src/main/java/com/bnp/bookstore/BookstoreAPIApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAPIApplication.class, args);
+	}
+
+}
