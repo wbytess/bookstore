4e5cdbf5ced8ad1b0a3af52aa73372bf3c247cde
Muhammad imran <iimrankhn@gmail.com>
2026-02-02 22:20:47 +0100

    test: should find all books with all columns [GREEN]

--- bookstore-api/src/test/java/com/bnp/bookstore/repository/BookRepositoryTest.java
+++ bookstore-api/src/test/java/com/bnp/bookstore/repository/BookRepositoryTest.java
@@ -2,11 +2,15 @@ package com.bnp.bookstore.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import com.bnp.bookstore.model.Book;
 
@@ -15,6 +19,9 @@ class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+    
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     @DisplayName("should create a book with all columns")
@@ -27,6 +34,20 @@ class BookRepositoryTest {
         assertEquals("Test Book", savedBook.getName());
         assertEquals("Test Author", savedBook.getAuthor());
         assertEquals(10.50, savedBook.getPrice());
+    }
+    
+    @Test
+    @DisplayName("should find all books with all columns")
+    void shouldFindAllBooks() {
+        bookRepository.save(new Book(null, "Book 1", "Author 1", 10.99));
+        bookRepository.save(new Book(null, "Book 2", "Author 2", 20.99));
+        entityManager.flush();
+
+        List<Book> books = bookRepository.findAll();
+
+        assertEquals(2, books.size());
+        assertTrue(books.stream().anyMatch(b -> b.getName().equals("Book 1")));
+        assertTrue(books.stream().anyMatch(b -> b.getName().equals("Book 2")));
     }
 
 }
