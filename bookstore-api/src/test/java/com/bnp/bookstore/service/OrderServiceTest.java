187c5353fec830e73792d6f869b96dcfd8f8e358
Muhammad imran <iimrankhn@gmail.com>
2026-02-03 14:01:03 +0100

    test: should create an order with a single item having two books and return total amount

--- bookstore-api/src/test/java/com/bnp/bookstore/service/OrderServiceTest.java
+++ bookstore-api/src/test/java/com/bnp/bookstore/service/OrderServiceTest.java
@@ -1,11 +1,15 @@
 package com.bnp.bookstore.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +17,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.bnp.bookstore.model.Book;
 import com.bnp.bookstore.model.Order;
+import com.bnp.bookstore.model.OrderItem;
 import com.bnp.bookstore.repository.OrderRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +30,20 @@ class OrderServiceTest {
 
     @InjectMocks
     private OrderService orderService;
+    
+    private Book book;
+
+    @BeforeEach
+    void setup() {
+        book = new Book(1L, "New Book1", "New Author1", 10.00);
+
+        // lenient stub to prevent unnecessary stubbing exception
+        lenient().when(orderRepository.save(any(Order.class))).thenAnswer(invocation -> {
+            Order order = invocation.getArgument(0);
+            order.setId(1L); // simulate DB-generated ID
+            return order;
+        });
+    }
 
     @Test
     @DisplayName("should get an order by order id")
@@ -40,4 +60,26 @@ class OrderServiceTest {
         assertTrue(orderById.isPresent());
         assertEquals(10.50, orderById.get().getTotalAmount());
     }
+	
+	@Test
+	@DisplayName("should create an order with a single item having two books and return total amount")
+	void shouldCreateAnOrder() {
+		Order order = new Order();
+
+        OrderItem item = new OrderItem();
+        item.setOrder(order);
+        item.setBook(book);
+        item.setQuantity(2);
+
+        order.getOrderItems().add(item);
+
+        Order saved = orderService.saveOrder(order);
+
+        assertNotNull(saved);
+        assertNotNull(saved.getId());
+        assertEquals(1, saved.getOrderItems().size());
+        assertEquals(2, saved.getOrderItems().get(0).getQuantity());
+        assertEquals(20.0, saved.getTotalAmount());
+	}
+
 }
