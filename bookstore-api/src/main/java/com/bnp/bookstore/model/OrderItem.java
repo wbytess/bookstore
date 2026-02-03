63e3de478bbe574ff479611576bf56b5034d0cdc
Muhammad imran <iimrankhn@gmail.com>
2026-02-03 10:30:18 +0100

    feat: add OrderItem entity with book mapping

---
+++ bookstore-api/src/main/java/com/bnp/bookstore/model/OrderItem.java
@@ -0,0 +1,38 @@
+package com.bnp.bookstore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id", nullable = false)
+    @JsonIgnore
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Double getSubtotal() {
+        return price * quantity;
+    }
+}
