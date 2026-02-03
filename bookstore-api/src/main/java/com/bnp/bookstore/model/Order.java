413754fa82439ba8ab73c7b14582b2007d3ef4cf
Muhammad imran <iimrankhn@gmail.com>
2026-02-03 22:01:20 +0100

    feat: order status enum introduced to track order status

--- bookstore-api/src/main/java/com/bnp/bookstore/model/Order.java
+++ bookstore-api/src/main/java/com/bnp/bookstore/model/Order.java
@@ -4,16 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,6 +25,10 @@ public class Order {
     
     @Column(name = "order_date", nullable = false)
     private LocalDateTime orderDate;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
     
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<OrderItem> orderItems = new ArrayList<>();
---
+++ bookstore-api/src/main/java/com/bnp/bookstore/model/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.bnp.bookstore.model;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    CANCELLED
+}
